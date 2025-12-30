1b55705e9c1d0a321ec9ff02deab980d4df4740c
ShrayRK <shray.works@gmail.com>
2025-12-30 16:51:08 +0530

    cor update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Tag = require("./models/tag.models");
 dbConnection();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -21,7 +21,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.options("*", cors(corsOptions));
+app.options("/", cors(corsOptions));
 
 async function addLead(newLead) {
   const lead = new Lead(newLead);
