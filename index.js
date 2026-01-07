0341adae950794c2937e803776f1651b4a56a053
ShrayRK <shray.works@gmail.com>
2026-01-07 11:22:09 +0530

    cors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.options("*", cors(corsOptions));
+app.options("/", cors(corsOptions));
 
 async function addLead(newLead) {
   const lead = new Lead(newLead);
